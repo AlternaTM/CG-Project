921414199f07af2283ff5b5579b98187a71ac28b
ErNickIlSommo <nick.gianuzzi99@gmail.com>
2025-12-09 18:20:19 +0100

    remove input files

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,45 @@
 
 #include <iostream>
 
+
 int main() {
-    std::cout << "Ciao Mondo" << std::endl;
+    
+    GLFWwindow* window;
+    if (!glfwInit())
+        return -1;
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow(650, 500, "Project", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+    
+    // Add callback for resizing
+    // ...
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to init GLAD" << std::endl;
+        return -1;
+    }
+
+    /* Render Loop */
+    while (!glfwWindowShouldClose(window))
+    {
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glfwSwapBuffers(window);
+
+        glfwPollEvents();
+    }
+
     return 0;
 }
